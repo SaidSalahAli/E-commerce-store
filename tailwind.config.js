342f6da8a6dcb55f71ddbd0b4638e81032004df9
SaidSalahAli <saidsalah375@gmail.com>
2023-12-23 18:32:51 +0200

    commit create search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      height:{
+        header:"560px",
+        rate: "400px",
+      },
+      fontSize:{
+        h1:"2.6rem",
+      },
+      screens:{
+        xs:"475px"
+      },
+      colors:{
+        main: '#fbfbfd',
+        subMain: '#f20000',
+        dry: '#4a4e69',
+        star: '#ffb000',
+        text: '#eeee',
+        border: '#4d5563',
+        dryGray: '#c0c0c0',
+
+      },
+    },
   },
   plugins: [],
-};
+}
